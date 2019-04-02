01cfb643c3a3de97b2cce3ff6ded1576e31edd0a
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-02 16:09:36 -0700

    mvp done

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,13 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function outerLayer() {
+  const funTimes = 'JS is hard but fun!';
+  function innerLayer() {
+    return `${ funTimes }`
+  }
+  console.log(innerLayer()); // Works here!
+}
+console.log(outerLayer()); // But not here!
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
