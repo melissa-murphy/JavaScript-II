1ca74dc76679126591cc2952d7580cc6b041ea06
Melissa Murphy <melissa@thoughtfulmonkey.design>
2019-04-02 17:27:34 -0700

    fixed spelling errors

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -458,14 +458,14 @@ const runners = [
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName.
 let fullName = [];
-runners.forEach(runner => fullName.push(`${runner.first_name} ${runner.last_name}`));
+runners.forEach(runners => fullName.push(`${runners.first_name} ${runners.last_name}`));
 
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
-allCaps = runners.map(runner => runner.first_name.toUpperCase());
+allCaps = runners.map(runners => runners.first_name.toUpperCase());
 console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
@@ -488,16 +488,16 @@ console.log(ticketPriceTotal);
 // Problem 1
 // The companies all want their name in caps
 let companyNames = [];
-companyNames = runners.map(runner => runner.company_name.toUpperCase());
+companyNames = runners.map(runners => runners.company_name.toUpperCase());
 console.log(companyNames);
 
 // Problem 2
 // Starting order alphabetically by last name
 let runningOrder = [];
-runners.forEach(runner => runningOrder.push(`${runner.last_name}`));
+runners.forEach(runners => runningOrder.push(`${runners.last_name}`));
 console.log(runningOrder.sort());
 // Problem 3
 // Create mailing list for runners
 let runnerEmailList = [];
-runners.forEach(runner => runnerEmailList.push(`${runner.email}`));
-console.log(runnerEmailList);
+runners.forEach(runners => runnersEmailList.push(`${runners.email}`));
+console.log(runnersEmailList);
